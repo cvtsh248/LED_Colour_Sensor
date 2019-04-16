4bc3c0bf7870fcc9e96096ad4ca1d8e8746c7f8b
Shourjo Chakravarty <cvtsh248@gmail.com>
2019-04-16 16:05:32 +0800

    Add files via upload
    Main program

---
+++ LED_TEST.ino
@@ -0,0 +1,32 @@
+int red = A0;
+int green = A1;
+int blue = A2; 
+//red, 235; green, 260; blue, 330; 
+void setup() {
+  Serial.begin(9600);
+}
+
+int average_led_reading(int pin){
+  long avg = 0;
+  for(int i = 0; i < 500; i++){
+    avg += analogRead(pin);
+  }
+  return avg/500;
+}
+void loop() {
+  float r = float(average_led_reading(red));
+  float g = float(average_led_reading(green));
+  float b = float(average_led_reading(blue));
+  float rel_r = (r/235)*255;
+  float rel_g = (g/260)*255;
+  float rel_b = (b/330)*255;
+  /*
+  Serial.println(int(round(rel_r)));
+  Serial.println(int(round(rel_g)));
+  Serial.println(int(round(rel_b)));
+  */
+  Serial.println((String)"r:"+int(round(rel_r))+" g:"+int(round(rel_g))+" b:"+int(round(rel_b)));
+  Serial.println('\n');
+  delay(1000);
+  
+}
